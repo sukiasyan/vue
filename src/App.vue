dbe2a673e3d83ef9c44143e8196fa819c1bedcce
hsukiasyan <h.sukiasyan@gethatch.com>
2022-11-08 12:24:41 +0100

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
-<template>
-  <SignUp />
-</template>
+<template><router-view /></template>
 
 <script>
-import SignUp from './components/SignUp.vue';
 export default {
   name: 'App',
-  components: {
-    SignUp,
-  },
 };
 </script>
 
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,15 +2,39 @@
   <img class="logo" alt="Vue logo" src="../assets/logo.png" />
   <h1>Sign Up</h1>
   <div class="register">
-    <input type="text" placeholder="Enter name" />
-    <input type="text" placeholder="Enter email" />
-    <input type="password" placeholder="Enter password" />
-    <button class="button">Sign Up</button>
+    <input type="text" v-model="name" placeholder="Enter name" />
+    <input type="text" v-model="email" placeholder="Enter email" />
+    <input type="password" v-model="password" placeholder="Enter password" />
+    <button v-on:click="signUp" class="button">Sign Up</button>
   </div>
 </template>
 
 <script>
-export default { name: 'SignUp' };
+import axios from 'axios';
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async signUp() {
+      let result = await axios.post('http://localhost:3000/users', {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      });
+      console.warn(result);
+      if (result.status === 201) {
+        localStorage.setItem('user-info', JSON.stringify(result.data));
+        this.$router.push({ name: 'Home' });
+      }
+    },
+  },
+};
 </script>
 
 <style>
