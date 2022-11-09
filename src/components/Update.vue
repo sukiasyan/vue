b527e6b529fc93383d0f4839531bf5e59ebb9d41
hsukiasyan <h.sukiasyan@gethatch.com>
2022-11-09 07:52:21 +0100

    update restaurants

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -1,19 +1,71 @@
 <template>
   <Header />
   <h1>Hello User, Welcome to Update Page</h1>
+  <form class="add">
+    <input
+      type="text"
+      name="name"
+      placeholder="Enter Name"
+      v-model="restaurant.name"
+    />
+    <input
+      type="text"
+      name="address"
+      placeholder="Enter Address"
+      v-model="restaurant.address"
+    />
+    <input
+      type="text"
+      name="contact"
+      placeholder="Enter Contact"
+      v-model="restaurant.contact"
+    />
+    <button type="button" v-on:click="updateRestaurant">
+      Update Restaurant
+    </button>
+  </form>
 </template>
 <script>
 import Header from './Header.vue';
+import axios from 'axios';
 export default {
   name: 'UpdateRestaurante',
   components: {
     Header,
   },
-  mounted() {
+  data() {
+    return {
+      restaurant: {
+        name: '',
+        address: '',
+        contact: '',
+      },
+    };
+  },
+  methods: {
+    async updateRestaurant() {
+      const result = await axios.put(
+        'http://localhost:3000/restaurant/' + this.$route.params.id,
+        {
+          name: this.restaurant.name,
+          address: this.restaurant.address,
+          contact: this.restaurant.contact,
+        }
+      );
+      if (result.status === 200) {
+        this.$router.push({ name: 'Home' });
+      }
+    },
+  },
+  async mounted() {
     let user = localStorage.getItem('user-info');
     if (!user) {
       this.$router.push({ name: 'SignUp' });
     }
+    const result = await axios.get(
+      'http://localhost:3000/restaurant/' + this.$route.params.id
+    );
+    this.restaurant = result.data;
   },
 };
 </script>
